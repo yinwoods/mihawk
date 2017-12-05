94e856bd2d7d763c1005b291da19f3ce122f9850
Yin Chengtao <yinchengtao@4paradigm.com>
2017-12-05 18:57:00 +0800

    add restart to mysql

--- bin/start_mysql.sh
+++ bin/start_mysql.sh
@@ -1,1 +1,1 @@
-docker run -p 3306:3306 --name mihawk-mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
+docker run -p 3306:3306 --restart on-failure:10 --name mihawk-mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
