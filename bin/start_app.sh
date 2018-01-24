fc1bfc0fc6d0391023f63df03ece98ab42e5d5b1
yinwoods <yinwoods@163.com>
2018-01-24 16:21:45 +0800

    change start app script

--- bin/start_app.sh
+++ bin/start_app.sh
@@ -1,2 +1,12 @@
 export PYTHONPATH="$HOME:$PYTHONPATH"
-screen -dmUS apistar apistar run --port 22399 --host 0.0.0.0
+gunicorn \
+    -w 10 \
+    --timeout 120 \
+    -b 0.0.0.0:22399 \
+    --limit-request-line 0 \
+    --access-logfile $HOME/mihawk/logs/access.log \
+    --error-logfile $HOME/mihawk/logs/error.log \
+    --log-level info \
+    --worker-class=meinheld.gmeinheld.MeinheldWorker \
+    -D \
+    app:app
