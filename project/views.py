b0d51c6ddeeb2ab42351841e2037b9f84e106fc6
yinwoods <yinwoods@163.com>
2018-02-28 18:57:19 +0800

    fix bug

--- project/views.py
+++ project/views.py
@@ -22,18 +22,16 @@ def alert(params: http.QueryParams):
     metric = params["metric"].strip()
     tags = params["tags"].strip().replace(":", "=")
 
-    title = f'{endpoint} {metric}报警'
-
     tpl_id = int(params["tpl_id"])
     exp_id = int(params["exp_id"])
     user_infos = dbapi.get_user_contact_by_tpl_id(tpl_id, exp_id)
 
     # 目前的415错误先过滤掉，等下一个版本发布后再恢复
     if tags == "api=__serv__,errcode=415" or tags == "api=/dangdang/api/config,errcode=415":
-        return {"mail": "misstatement", "sms": "misstatement", "im": "misstatement"}
+        return {"sms": "misstatement", "im": "misstatement"}
     # 目前的400错误先过滤掉，等待世举查明原因
     if tags == "api=__serv__,errcode=400" or tags == "api=/dangdang/api/log,errcode=400":
-        return {"mail": "misstatement", "sms": "misstatement", "im": "misstatement"}
+        return {"sms": "misstatement", "im": "misstatement"}
 
     metric = metric + "/" + tags
 
@@ -41,7 +39,7 @@ def alert(params: http.QueryParams):
     event_infos = dbapi.get_infos_by_endpoint_metric_time(endpoint, metric, interval=10)
 
     if len(event_infos) == 0:
-        return {"mail": "misstatement", "sms": "misstatement", "im": "misstatement"}
+        return {"sms": "misstatement", "im": "misstatement"}
 
     event_info = event_infos[0]
 
@@ -51,25 +49,17 @@ def alert(params: http.QueryParams):
     ims = [user[3] for user in user_infos]
     ims = ",".join(ims)
 
-    path = project_config["path"]
-    with open(f"{path}/templates/alert.tmpl", "r") as f:
-
-        t = "".join(f.readlines())
-        t = Template(t)
-
-        html_message = t.render(params=event_info)
-
-        # 仅当10分钟内相同报警出现3次或3次以上才会触发短信报警
-        if event_info[4] >= 3:
-            response = {
-                "sms": send_sms(event_info, phones),
-                "im": send_wechat(event_info, ims)
-            }
-        else:
-            response = {
-                "sms": "misstatement",
-                "im": send_wechat(event_info, ims)
-            }
+    # 仅当10分钟内相同报警出现3次或3次以上才会触发短信报警
+    if event_info[4] >= 3:
+        response = {
+            "sms": send_sms(event_info, phones),
+            "im": send_wechat(event_info, ims)
+        }
+    else:
+        response = {
+            "sms": "misstatement",
+            "im": send_wechat(event_info, ims)
+        }
 
     return response
 
