338c226c8960dc419153cd3ab7651f2cc2833eec
yinwoods <yinwoods@163.com>
2018-02-28 17:12:28 +0800

    fix

--- project/routes.py
+++ project/routes.py
@@ -8,7 +8,7 @@ from mihawk.project.views import notify_email
 from mihawk.project.views import notify_sms
 
 routes = [
-    Route("/mail", "POST", email.notify),
+    Route("/email", "POST", email.notify),
     Route("/alert", "GET", alert),
     Route("/notify/email", "POST", notify_email),
     Route("/notify/sms", "POST", notify_sms),
